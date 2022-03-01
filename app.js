4bd0803ea7fc45fdb2f16e621b8bb67127418919
Kaspersyv <kaspersyv@viken.no>
2022-03-01 14:51:05 +0100

    Update

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
-   
-      let myStorage = window.sessionStorage;
-      
-      let userInput = JSON.stringify(document.querySelector("#userInput"))
+   let myStorage = window.sessionStorage;
 
-      function welcome() {
-        sessionStorage.setItem("name", userInput)
-        console.log(userInput)
-        const userName = sessionStorage.getItem("name")
-        $("#nameField").innerText = "Velkommen " + userName; 
-      }
+   let userInput = JSON.stringify(document.querySelector("#userInput"))
 
-  
-      const userName = myStorage.getItem("name")
-
-      let current = new Date();
-
-      let firstRandom = Math.floor(Math.random() * 8) + 1;
-
-      let lastMinute = new Date().getMinutes();
-
-      const checkMinute = () => {
-          let randomNumber = Math.floor(Math.random() * 8) + 1;
-          thisMinute = new Date().getMinutes();
-          if (thisMinute !== lastMinute) {
-              lastMinute = thisMinute
-              document.querySelector("#clockField").innerText = "Ditt lykketall: " + randomNumber
-          }
-
-         
+   function welcome() {
+       sessionStorage.setItem("name", userInput)
+       console.log(userInput)
+       const userName = sessionStorage.getItem("name")
+       $("#nameField").innerText = "Velkommen " + userName;
+   }
 
 
-      }
+   const userName = myStorage.getItem("name")
 
-      
+   let current = new Date();
+
+   let firstRandom = Math.floor(Math.random() * 8) + 1;
+
+   let lastMinute = new Date().getMinutes();
+
+   const checkMinute = () => {
+       let randomNumber = Math.floor(Math.random() * 8) + 1;
+       thisMinute = new Date().getMinutes();
+       if (thisMinute !== lastMinute) {
+           lastMinute = thisMinute
+           document.querySelector("#clockField").innerText = "Ditt lykketall er: " + randomNumber
+       }
 
 
-      window.setInterval(checkMinute, 1000);
-      window.setInterval(checkGuess, 1000);
-      document.getElementById("clockField").innerText = "Your lucky number is: " + firstRandom
+
+
+   }
+
+
+
+
+   window.setInterval(checkMinute, 1000);
+   document.getElementById("clockField").innerText = "Your lucky number is: " + firstRandom
+
+
